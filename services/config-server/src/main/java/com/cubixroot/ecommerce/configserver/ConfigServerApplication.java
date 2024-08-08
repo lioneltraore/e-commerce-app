ce0af286d86edfabeff7a31c1b985c028bc7f29b
Lionel Traore <hello@lioneltraore.com>
2024-08-08 02:17:17 +0000

    add config-server ms

---
+++ services/config-server/src/main/java/com/cubixroot/ecommerce/configserver/ConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.cubixroot.ecommerce.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+}
